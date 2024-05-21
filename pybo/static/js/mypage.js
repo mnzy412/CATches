0fb665eb60cf03fdd3e89ab98f006c7d1ea207a6
kimmnzy <mnzy412@gmail.com>
2024-05-21 16:30:06 +0900

    [mypage] 고객센터, 문의 함수 수정

--- pybo/static/js/mypage.js
+++ pybo/static/js/mypage.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let customerSupportWindow = null;
+    let customerQuestionWindow = null;
+
     document.getElementById('customer-support-link').addEventListener('click', function(event) {
         event.preventDefault(); // 링크의 기본 동작을 막음
-        window.open('', '고객센터', 'width=400,height=200').document.write('<h1>이것은 고객센터다.</h1>');
+
+        if (!customerSupportWindow || customerSupportWindow.closed) {
+            customerSupportWindow = window.open('', '고객센터', 'width=400,height=200');
+        }
+
+        customerSupportWindow.document.body.innerHTML = '<h1>이것은 고객센터다.</h1>';
     });
 
     document.getElementById('customer-question-link').addEventListener('click', function(event) {
         event.preventDefault(); // 링크의 기본 동작을 막음
-        window.open('', '문의 안내', 'width=400,height=200').document.write('<h1>이메일 보내라: mnzy412@gmail.com</h1>');
+
+        if (!customerQuestionWindow || customerQuestionWindow.closed) {
+            customerQuestionWindow = window.open('', '문의 안내', 'width=400,height=200');
+        }
+
+        customerQuestionWindow.document.body.innerHTML = '<h1>이메일 보내라: mnzy412@gmail.com</h1>';
     });
 });
