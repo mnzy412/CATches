b97deae7575d150f2e4c8276b54e54a29c76297b
kimmnzy <mnzy412@gmail.com>
2024-05-26 04:06:48 +0900

    [회원가입/로그인] 1차 기능 구현

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
+import secrets
 
 def create_app():
     app = Flask(__name__)
+  # 임의의 고유한 secret_key 생성
+    app.secret_key = secrets.token_urlsafe(16) 
 
     # 블루프린트
     from .views import main_views
