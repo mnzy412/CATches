e06906fabef8f21ce18eeb1289ff8d4b89d75d40
kimmnzy <mnzy412@gmail.com>
2024-05-26 11:35:22 +0900

    [로그인] flash 메시지 위치 조정

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -3,7 +3,8 @@ import secrets
 
 def create_app():
     app = Flask(__name__)
-  # 임의의 고유한 secret_key 생성
+    
+    # 임의의 고유한 secret_key 생성
     app.secret_key = secrets.token_urlsafe(16) 
 
     # 블루프린트
