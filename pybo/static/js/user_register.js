d0488ad6bad47d1d3bd1922ff8c736669eb1ba08
kimmnzy <mnzy412@gmail.com>
2024-06-03 17:11:07 +0900

    [회원가입] js 오류 수정

--- pybo/static/js/user_register.js
+++ pybo/static/js/user_register.js
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('loginForm');
     const submitBtn = document.getElementById('submitBtn');
+    const nameInput = document.getElementById('name');
+    const phoneInput = document.getElementById('phone');
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
 
     function validateForm() {
-        if (emailInput.value && passwordInput.value) {
+        if (nameInput.value && phoneInput.value && emailInput.value && passwordInput.value) {
             submitBtn.disabled = false;
         } else {
             submitBtn.disabled = true;
         }
     }
 
+    nameInput.addEventListener('input', validateForm);
+    phoneInput.addEventListener('input', validateForm);
     emailInput.addEventListener('input', validateForm);
     passwordInput.addEventListener('input', validateForm);
 
